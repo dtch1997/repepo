3abba55ecb2b99dca1bf977641b048ae5948ff3c
Daniel Tan <25474937+dtch1997@users.noreply.github.com>
2023-11-01 17:05:31 +0000

    SFT baseline (#5)
    * Update requirements
    
    * Add SFT algorithm
    
    * Add datasets, log dirs to gitignore
    
    * Demonstrate how to configure dataset
    
    * Update README
    
    ---------
    
    Co-authored-by: aengusl <aenguslynch@gmail.com>
    Co-authored-by: Daniel CH Tan <dtch1997@users.noreply.github.com>

---
+++ repepo/baselines/README.md
@@ -0,0 +1,28 @@
+# Running Baselines
+
+The following code assumes you have set up dependencies.
+
+For detailed instructions, refer to README.md in top-level directory.
+
+## Generate datasets
+
+```bash
+cd repepo/data
+make datasets
+```
+
+Currently, we have implemented `stereoset` and `truthful_qa`.
+
+The datasets are stored in `datasets` in top-level directory.
+
+## Supervised Fine-tuning
+
+Our implementation is based off Stanford Alpaca.
+
+```bash
+python repepo/baselines/sft/train.py --num_train_epochs 30
+# --dataset_name truthful_qa (truthful_qa, steroset)
+# --model_name EleutherAI/Pythia70m (any HF model)
+```
+
+Watch the loss go down in WandB!
---
+++ repepo/baselines/sft/train.py
@@ -0,0 +1,90 @@
+#    Copyright 2023 Rohan Taori, Ishaan Gulrajani, Tianyi Zhang, Yann Dubois, Xuechen Li
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Sequence
+
+import transformers
+from transformers import Trainer
+
+from repepo.data import utils, get_dataset
+from repepo.data.dataset import sft
+from repepo.variables import Environ, Model
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default=Model.Pythia70m)
+
+@dataclass
+class DataArguments:
+    dataset_name: str = field(default="truthfulqa", metadata={"help": "Name of training dataset. See repepo.data.list_datasets() for details"})
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=Environ.HuggingfaceCacheDir)
+    output_dir: Optional[str] = field(default=Environ.OutputDir)
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(
+        default=512,
+        metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
+    )
+
+def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args: DataArguments) -> Dict:
+    """Make dataset and collator for supervised fine-tuning."""
+    list_data_dict = get_dataset(data_args.dataset_name)
+    train_dataset = sft.SupervisedDataset(list_data_dict, tokenizer=tokenizer)
+    data_collator = sft.DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
+
+
+
+def train():
+    # import logging
+    # loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    # for logger in loggers:
+    #     logger.setLevel(logging.INFO)
+    parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+    )
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+        use_fast=True,
+    )
+
+    special_tokens_dict = utils.get_pad_token(tokenizer)
+    special_tokens_dict.update(utils.get_special_tokens(tokenizer))
+    utils.smart_tokenizer_and_embedding_resize(
+        special_tokens_dict=special_tokens_dict,
+        tokenizer=tokenizer,
+        model=model,
+    )
+    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+    
+    # Train
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(output_dir=training_args.output_dir)
+
+
+if __name__ == "__main__":
+    train()
