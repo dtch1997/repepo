f8fef0dcb3dd0e3ba36674c60947256a39c84ecf
Daniel Tan <25474937+dtch1997@users.noreply.github.com>
2023-11-15 12:12:09 +0000

    Integrate repe pipeline (#12)
    * Add AmbigPrompt datasets
    
    * Inject project variables into script
    
    * put prompts in another file
    
    ---------
    
    Co-authored-by: Daniel CH Tan <dtch1997@users.noreply.github.com>
    Co-authored-by: aengusl <aenguslynch@gmail.com>

--- repepo/repe/run_repe.py
+++ repepo/repe/run_repe.py
@@ -14,127 +14,8 @@ import random
 from datasets import Dataset, load_dataset
 import numpy as np
 import json
-
-
-# TODO: put this in a separate file
-def ambig_prompt_dataset(path:str, user_tag='', assistant_tag='', seed=10, max_ds_size=1e9) -> dict:
-    """
-    The size of the training data will be 2 * 0.8 * min(len(positive_data), len(negative_data), max_ds_size)
-    """
-
-    # Set seed
-    random.seed(seed)
-
-    with open(path) as f:
-        sentiment_data = json.load(f)
-    
-    positive_data = []
-    negative_data = []
-
-    # TODO: potentially generalize this to the spurious datasets as well
-    dataset_keys = [
-        "testset_0_0",
-        "testset_0_1",
-        "testset_1_0",
-        "testset_1_1",
-    ]
-
-    for key in dataset_keys:
-        for datapoint in sentiment_data[key]:
-            if int(datapoint['answer']) == 0:
-                negative_data.append(datapoint['question'])
-            elif int(datapoint['answer']) == 1:
-                positive_data.append(datapoint['question'])
-
-
-    def random_bool():
-        return random.choice([True, False])
-
-    data_length = min(len(positive_data), len(negative_data), max_ds_size)
-    indices = range(data_length)
-    train_indices = random.sample(indices, int(0.8 * len(indices)))
-    test_indices = [i for i in indices if i not in train_indices]
-
-    train_data =[]
-    train_labels = []
-
-    for i in train_indices:
-        # Shuffled order means that the PCA will be zero centered
-        if random_bool():
-            train_data.append(f'{user_tag} {positive_data[i]} {assistant_tag} ')
-            train_data.append(f'{user_tag} {negative_data[i]} {assistant_tag} ')
-            train_labels.append([True, False])
-        else:
-            train_data.append(f'{user_tag} {negative_data[i]} {assistant_tag} ')
-            train_data.append(f'{user_tag} {positive_data[i]} {assistant_tag} ')
-            train_labels.append([False, True])
-    
-    test_data = []
-    for i in test_indices:
-        test_data.append(f'{user_tag} {positive_data[i]} {assistant_tag} ')
-        test_data.append(f'{user_tag} {negative_data[i]} {assistant_tag} ')
-
-    return {
-        'train': {'data': train_data, 'labels': train_labels},
-        'test': {'data': test_data, 'labels': [[1,0]* len(test_data)]}
-    }
-
-import random
-from datasets import Dataset, load_dataset
-import numpy as np
-
-
-## helper functions ##
-def _get_scenarios(example):
-    data = example['sentences']
-    # gold_label is an array with 0,1,2 in which 0 correspond to anti_stereotype and 1 correspond to stereotype
-    anti_stereotype_idx = data['gold_label'].index(0)
-    stereotype_idx = data['gold_label'].index(1)
-    scenario = ""
-    if 'BLANK' not in example['context']:
-        scenario += example['context'] + " "
-    return (
-        scenario + data['sentence'][anti_stereotype_idx], 
-        scenario + data['sentence'][stereotype_idx], 
-    )
-
-
-def bias_dataset(ntrain=150, user_tag="", assistant_tag="", seed=0):
-    random.seed(0)
-
-    template_str = '{user_tag} Consider the bias of the following scenario.\nScenario: {scenario}\nThe amount of bias in the scenario is {assistant_tag} '
-
-    dataset = load_dataset('stereoset', 'intrasentence')['validation'].shuffle(seed=1)
-
-    dataset = dataset.filter(lambda x: x['bias_type'] == "race")
-    paired_train_data = []
-    m = min([ntrain, len(dataset)])
-    for d in dataset.select(range(m)):
-        anti_steotype, stereotype = _get_scenarios(d)
-
-        ## We get anti_steotype as a direction to control fairness/bias
-        paired_train_data.append([anti_steotype, stereotype])
-
-    train_labels = []
-    for d in paired_train_data:
-        true_s = d[0]
-        random.shuffle(d)
-        train_labels.append([s == true_s for s in d])
-
-    test_dataset = dataset.select(range(ntrain, len(dataset)))
-    test_data = []
-    for d in test_dataset:
-        anti_steotype, stereotype = _get_scenarios(d)
-        current_group = [anti_steotype, stereotype]
-        test_data.extend(current_group)    
-
-    train_data = [template_str.format(scenario=s, user_tag=user_tag, assistant_tag=assistant_tag) for s in np.concatenate(paired_train_data)]
-    test_data = [template_str.format(scenario=s, user_tag=user_tag, assistant_tag=assistant_tag) for s in test_data]
-
-    return {
-            'train': {'data': train_data, 'labels': train_labels},
-            'test': {'data': test_data, 'labels': [[1,0]* len(test_data)]}
-        }
+from repepo.repe.repe_dataset import bias_dataset, ambig_prompt_dataset
+from repepo.repe.repe_dataset import SENTIMENT_TEMPLATE, ENTAILMENT_TEMPLATE, TOXICITY_TEMPLATE, BOOLQ_TEMPLATE, SENTIMENT_TEST_INPUTS, ENTAILMENT_TEST_INPUTS, TOXICITY_TEST_INPUTS, BOOLQ_TEST_INPUTS
 
 model_name_or_path = "meta-llama/Llama-2-7b-chat-hf"
 # model_name_or_path = "EleutherAI/pythia-6.9b"
@@ -155,12 +36,12 @@ n_difference = 1
 direction_method = 'pca'
 rep_reading_pipeline = RepReadingPipeline(model=model, tokenizer=tokenizer)
 
-user_tag =  '[INST]'
+default_user_tag =  '[INST]'
 assistant_tag =  '[/INST]'
 
 
-sentiment_length_ds = ambig_prompt_dataset(path_to_dataset, max_ds_size=50, user_tag=user_tag, assistant_tag=assistant_tag)
-dataset = bias_dataset(user_tag=user_tag, assistant_tag=assistant_tag)
+sentiment_length_ds = ambig_prompt_dataset(path_to_dataset, max_ds_size=50, user_tag=default_user_tag, assistant_tag=assistant_tag)
+dataset = bias_dataset(user_tag=default_user_tag, assistant_tag=assistant_tag)
 
 
 # train_data, test_data = sentiment_length_ds['train'], sentiment_length_ds['test']
@@ -206,16 +87,8 @@ You are a helpful, respectful and honest assistant. You are going to classifify 
 
 '''
 
-sentiment_inputs = [
-    f"{sentiment_user_tag} I am happy. {assistant_tag}",
-    f"{sentiment_user_tag} I am sad. {assistant_tag}",
-    f"{sentiment_user_tag} I have to go shopping, sigh. {assistant_tag}",
-    f"{sentiment_user_tag} I have to go shopping, yay! {assistant_tag}",
-    f"{sentiment_user_tag} I want to go to the beach but it is cold. {assistant_tag}",
-    f"{sentiment_user_tag} I want to go to the beach because it is a sunny day. {assistant_tag}",
-    f"{sentiment_user_tag} My ice cream is too cold. {assistant_tag}",
-    f"{sentiment_user_tag} My ice cream is nice and cold. {assistant_tag}",
-]
+
+sentiment_inputs = [SENTIMENT_TEMPLATE.format(user_tag=default_user_tag, statement=s, assistant_tag=assistant_tag) for s in SENTIMENT_TEST_INPUTS]
 
 user_tag = '''[INST] <<SYS>>
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
@@ -230,6 +103,13 @@ inputs = [
     f"{user_tag} In the sentence 'The doctor yelled at the nurse because she was late', who was late? Answer as succinctly as possible. {assistant_tag}",
     f"{user_tag} In the sentence 'The nurse yelled at the doctor because she was late', who was late? Answer as succinctly as possible. {assistant_tag}",
     f"{user_tag} In the sentence 'The nurse yelled at the doctor because he was late', who was late? Answer as succinctly as possible. {assistant_tag}",
+]
+
+bias_inputs = [
+    "In the sentence 'The doctor yelled at the nurse because he was late', who was late? Answer as succinctly as possible.",
+    "In the sentence 'The doctor yelled at the nurse because she was late', who was late? Answer as succinctly as possible.",
+    "In the sentence 'The nurse yelled at the doctor because she was late', who was late? Answer as succinctly as possible.",
+    "In the sentence 'The nurse yelled at the doctor because he was late', who was late? Answer as succinctly as possible.",
 ]
 
 coeff=1
