a0cd3bcbeb5a72eef91a35a8bd9c27c163543b6b
dtch1997 <dtch1997@users.noreply.github.com>
2023-12-23 17:50:51 +0000

    customize wandb logging

--- repepo/algorithms/sft.py
+++ repepo/algorithms/sft.py
@@ -11,8 +11,11 @@ from typing import List, Optional
 import torch
 import pprint
 from torch.utils.data import DataLoader
+from transformers.trainer import Trainer
 from transformers.optimization import AdamW
 from transformers.optimization import get_linear_schedule_with_warmup
+from transformers.training_args import TrainingArguments as HfTrainingArguments
+
 from repepo.core.types import Completion
 
 from repepo.data import make_dataset, DatasetSpec
@@ -22,18 +25,14 @@ from repepo.utils.metrics import Metrics, AverageMeter
 from repepo.variables import Environ
 from repepo.variables import Model
 from repepo.utils import logging
+from repepo.utils.logging import WandbLogger, WandbConfig
 
+from typing import Any
 
 @dataclass
-class SupervisedFineTuningConfig:
-    # Training config
-    batch_size: int = 256  # Training batch size
-    shuffle: bool = True  # Whether to shuffle the dataset
-    num_train_epochs: int = 10
-    learning_rate: float = 5e-5
-
-    # Experiment config
-    device: str = "cuda"
+class SupervisedFineTuningConfig(HfTrainingArguments):
+    output_dir: Optional[str] = field(default=Environ.OutputDir)
+    optim: str = field(default="adamw_torch")
 
 
 class SupervisedFineTuning(Algorithm):
@@ -45,79 +44,33 @@ class SupervisedFineTuning(Algorithm):
         self,
         pipeline: Pipeline,
         dataset: Dataset,
-        logger: Optional[logging.Logger] = None,
-    ) -> Pipeline:
-        """Modifies the base model weights"""
+    ) -> dict[str, Any]:
+        """Modifies the base model weights
+        
+        Returns a dict of metrics """
 
-        # Load model and tokenizer
-        model = pipeline.model
+        # Set padding token if necessary
         tokenizer = pipeline.tokenizer
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
 
-        # Add new tokens to tokenizer
-        # This is because many tokenizers don't have a padding token
-        special_tokens_dict = utils.get_pad_token(tokenizer)
-        special_tokens_dict.update(utils.get_special_tokens(tokenizer))
-        utils.smart_tokenizer_and_embedding_resize(
-            special_tokens_dict=special_tokens_dict,
-            tokenizer=tokenizer,
-            model=model,
-        )
-
-        # Make train dataloader
+        # Make dataset
         completions: List[Completion] = pipeline.formatter.apply_list(dataset)
-        _ds = sft.SupervisedDataset(completions, tokenizer=tokenizer)
+        train_dataset = sft.SupervisedDataset(completions, tokenizer=tokenizer)
         data_collator = sft.DataCollatorForSupervisedDataset(tokenizer=tokenizer)
-        train_dataloader = DataLoader(
-            _ds,
-            batch_size=self.config.batch_size,
-            shuffle=self.config.shuffle,
-            collate_fn=data_collator,
+
+        trainer = Trainer(
+            model = pipeline.model, 
+            tokenizer = pipeline.tokenizer,
+            args = self.config,
+            train_dataset = train_dataset,
+            data_collator = data_collator, 
         )
+        trainer.train()
 
-        # Set device
-        device = self.config.device if torch.cuda.is_available() else "cpu"
-        model.to(device)  # type: ignore
-
-        # Set up optimizer and scheduler
-        num_training_steps = self.config.num_train_epochs * len(train_dataloader)
-        optimizer = AdamW(model.parameters(), lr=self.config.learning_rate)
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-        )
-
-        # Set up metrics, meters
-        metric_fns = Metrics()
-        meter = AverageMeter()
-
-        # Training loop
-        global_step = 0
-        model.train()
-        for epoch in range(int(self.config.num_train_epochs)):
-            # epoch_iterator = tqdm(train_dataloader, desc="Training")
-            epoch_iterator = train_dataloader
-            for step, batch in enumerate(epoch_iterator):
-                global_step += self.config.batch_size
-                batch = {k: v.to(device) for k, v in batch.items()}
-                outputs = model(
-                    input_ids=batch["input_ids"],
-                    labels=batch["labels"],
-                    attention_mask=batch["attention_mask"],
-                )
-                loss = outputs.loss
-                optimizer.zero_grad()
-                loss.backward()
-                # Gradient clipping
-                torch.nn.utils.clip_grad.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
-                scheduler.step()
-                print(f"epoch : {epoch} | step: {step} | loss: {loss}")
-                if logger is not None:
-                    logger.log({"train/loss": loss}, step=global_step)
-
-            # TODO: Evaluation callback?
-
-        return pipeline
-
+        return {
+            'loss_history': trainer.state.log_history,
+        }
 
 if __name__ == "__main__":
     import pyrallis
@@ -128,7 +81,7 @@ if __name__ == "__main__":
     @dataclass
     class TrainSFTConfig:
         sft: SupervisedFineTuningConfig = SupervisedFineTuningConfig()
-        dataset: DatasetSpec = DatasetSpec(name="truthfulqa")
+        dataset: DatasetSpec = DatasetSpec(name="stereoset", split=":80%", seed = 0)
         wandb: WandbConfig = WandbConfig()
 
         model_name_or_path: str = Model.Pythia70m
@@ -139,7 +92,6 @@ if __name__ == "__main__":
             },
         )
         cache_dir: str = Environ.HuggingfaceCacheDir
-        output_dir: str = Environ.OutputDir
 
     config = pyrallis.parse(TrainSFTConfig)
     pprint.pprint(config)
@@ -161,6 +113,6 @@ if __name__ == "__main__":
     pipeline = Pipeline(model, tokenizer)
     dataset = make_dataset(config.dataset)
 
-    with WandbLogger(config.wandb) as logger:
+    with WandbLogger(config.wandb):
         algorithm = SupervisedFineTuning(config.sft)
-        algorithm.run(pipeline, dataset, logger=logger)
+        algorithm.run(pipeline, dataset)
