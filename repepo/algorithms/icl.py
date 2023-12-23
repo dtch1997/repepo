83e4be76a3ea0cbbaa69726b3fd69726f753bcb1
dtch1997 <dtch1997@users.noreply.github.com>
2023-12-23 22:32:26 +0000

    Modify ICL to match new semantics

--- repepo/algorithms/icl.py
+++ repepo/algorithms/icl.py
@@ -1,14 +1,15 @@
-from dataclasses import replace
 from repepo.core.prompt import FewShotPrompter
 from .base import Algorithm
 from repepo.core import Pipeline, Dataset
+from typing import Any
 
 
 class InContextLearning(Algorithm):
-    def run(self, pipeline: Pipeline, dataset: Dataset) -> Pipeline:
+    def run(self, pipeline: Pipeline, dataset: Dataset) -> dict[str, Any]:
         """Uses an in-context learning prefix to prompts"""
 
         icl_completions = pipeline.formatter.apply_list(dataset)
         new_prompter = FewShotPrompter(icl_completions)
+        pipeline.prompter = new_prompter
 
-        return replace(pipeline, prompter=new_prompter)
+        return {}
