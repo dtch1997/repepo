856145eefe08731f7660ceab77046fa2fd55964e
David Chanin <chanindav@gmail.com>
2024-01-11 06:15:54 -0500

    limiting ICL max examples to avoid prepending entire dataset (#57)

--- repepo/algorithms/icl.py
+++ repepo/algorithms/icl.py
@@ -5,10 +5,18 @@ from repepo.core import Pipeline, Dataset
 
 
 class InContextLearning(Algorithm):
+    max_icl_examples: int
+
+    def __init__(self, max_icl_examples: int = 5):
+        super().__init__()
+        self.max_icl_examples = max_icl_examples
+
     def run(self, pipeline: Pipeline, dataset: Dataset) -> Pipeline:
         """Uses an in-context learning prefix to prompts"""
 
-        icl_completions = pipeline.formatter.apply_list(dataset)
+        icl_completions = pipeline.formatter.apply_list(
+            dataset[: self.max_icl_examples]
+        )
         new_prompter = FewShotPrompter(icl_completions)
 
         return replace(pipeline, prompter=new_prompter)
