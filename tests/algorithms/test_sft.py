09bad51468564c1d220962fa8ed3b3af3a31ec2c
dtch1997 <dtch1997@users.noreply.github.com>
2024-01-15 14:35:36 +0000

    Improve test cases

--- tests/algorithms/test_sft.py
+++ tests/algorithms/test_sft.py
@@ -22,9 +22,52 @@ def test_SupervisedFineTuning_run(
         Example(instruction="", input="Berlin is in", output="Germany"),
     ]
 
+    test_example = Example(
+        instruction="",
+        input="Paris is in",
+        output="France",
+        incorrect_outputs=["Germany", "Italy"],
+    )
+
     metrics = algorithm.run(pipeline, dataset=dataset)
     train_state_log_history = metrics["train_state_log_history"]
     final_train_state = train_state_log_history[-1]
 
     assert int(final_train_state["epoch"] == NUM_TRAIN_EPOCHS)
     assert final_train_state["train_loss"] < 0.1
+
+
+def test_SupervisedFineTuning_run_with_generate(
+    model: GPTNeoXForCausalLM, tokenizer: Tokenizer
+) -> None:
+    """
+    This test is the same as the one above, but it also runs generate() after fine-tuning.
+    NOTE: This currently doesn't induce the model to output 'B'.
+    In the future, we should improve the setup to enable this somehow
+    """
+    NUM_TRAIN_EPOCHS = 100
+
+    pipeline = Pipeline(model, tokenizer)
+    config = SupervisedFineTuningConfig(
+        num_train_epochs=NUM_TRAIN_EPOCHS,
+        report_to=[],
+    )
+    algorithm = SupervisedFineTuning(config)
+    dataset = [
+        Example(instruction="", input="The letter after A is ____", output="B"),
+    ]
+
+    test_example = Example(
+        instruction="", input="The letter after A is ____", output="B"
+    )
+
+    original_outputs = pipeline.generate(test_example)
+    metrics = algorithm.run(pipeline, dataset=dataset)
+    train_state_log_history = metrics["train_state_log_history"]
+    final_train_state = train_state_log_history[-1]
+
+    assert int(final_train_state["epoch"] == NUM_TRAIN_EPOCHS)
+    assert final_train_state["train_loss"] < 0.1
+
+    new_outputs = pipeline.generate(test_example)
+    assert original_outputs != new_outputs
