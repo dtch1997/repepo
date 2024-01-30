37c5e39f3805ff0049cb28c45de76c35b1db1e54
dtch1997 <dtch1997@users.noreply.github.com>
2024-01-30 09:56:32 +0000

    Add TQA translate

--- repepo/experiments/tqa_translate/translate.py
+++ repepo/experiments/tqa_translate/translate.py
@@ -1,46 +1,59 @@
+""" Translate TQA and save as CSV """
+
+import pandas as pd
+
 from arrr import translate
+from mosestokenizer import MosesTokenizer, MosesDetokenizer
+from repepo.data.make_dataset import make_dataset, DatasetSpec
+from repepo.core.types import Example
+from typing import Callable
+from dataclasses import asdict
+
+TranslationFn = Callable[[str], str]
 
 vowels = ["a", "e", "i", "o", "u"]
+punctuation = [".", ",", "!", "?", ";", ":"]
 
-# TODO: Use sentencepiece for tokenization
 
+def translate_to_pig_latin(english: str) -> str:
+    with MosesTokenizer("en") as tokenizer:
+        english_words = tokenizer(english)
 
-def translate_to_pig_latin(english_words: list[str]) -> list[str]:
     latin_words = []
 
     for word in english_words:
-        has_vowel = False
+        if len(word) == 0:
+            continue
 
-        for i in range(len(word)):
-            """
-            if first letter is a vowel
-            """
-            if word[0] in vowels:
-                latin_words.append(word + "-yay")
-                break
+        # If the word is a punctuation, continue
+        if word[0] in punctuation:
+            latin_words.append(word)
 
-            else:
-                """
-                else get vowel position and postfix all the consonants
-                present before that vowel to the end of the word along with "ay"
-                """
+        # If the word starts with a vowel then simply postfix "yay"
+        elif word[0] in vowels:
+            latin_words.append(word + "yay")
 
+        # If the word starts with a consonant, then find the first vowel
+        # and postfix all the consonants present before that vowel to the end
+        # of the word and postfix "ay"
+        else:
+            has_vowel = False
+            for i in range(len(word)):
                 if word[i] in vowels:
                     latin_words.append(word[i:] + word[:i] + "ay")
                     has_vowel = True
                     break
 
-                # if the word doesn't have any vowel then simply postfix "ay"
-                if (not has_vowel) and i == len(word) - 1:
-                    latin_words.append(word + "ay")
-                    break
+            # If the word does not have a vowel, then postfix "ay"
+            if not has_vowel:
+                latin_words.append(word + "ay")
 
-        raise RuntimeError("Should never reach here")
-
-    return latin_words
+    with MosesDetokenizer("en") as detokenizer:
+        return detokenizer(latin_words)
 
 
-def translate_to_leetspeak(english_words: list[str]) -> list[str]:
+def translate_to_leetspeak(english: str) -> str:
+    english_words = english.split(" ")
     leet_words = []
 
     for word in english_words:
@@ -60,10 +73,71 @@ def translate_to_leetspeak(english_words: list[str]) -> list[str]:
                 leet_word += letter
         leet_words.append(leet_word)
 
-    return leet_words
+    return " ".join(leet_words)
 
 
-def translate_to_pirate_speak(english_words: list[str]) -> list[str]:
-    english = " ".join(english_words)
-    pirate = translate(english)
-    return pirate.split(" ")
+def translate_to_pirate_speak(english: str) -> str:
+    return translate(english)
+
+
+def translate_example(example: Example, translation_func: TranslationFn) -> Example:
+    if example.incorrect_outputs is None:
+        return Example(
+            instruction=translation_func(example.instruction),
+            input=translation_func(example.input),
+            output=translation_func(example.output),
+            incorrect_outputs=None,
+        )
+
+    else:
+        incorrect_outputs_trans = [
+            translation_func(output) for output in example.incorrect_outputs
+        ]
+        return Example(
+            instruction=translation_func(example.instruction),
+            input=translation_func(example.input),
+            output=translation_func(example.output),
+            incorrect_outputs=incorrect_outputs_trans,
+        )
+
+
+if __name__ == "__main__":
+    import pathlib
+
+    current_dir = pathlib.Path(__file__).parent.absolute()
+
+    list_data = make_dataset(
+        DatasetSpec(
+            name="truthfulqa",
+        )
+    )[:100]
+
+    df_english = pd.json_normalize([asdict(example) for example in list_data])
+    df_english.to_csv(current_dir / "truthfulqa_english.csv", index=False)
+
+    # Translate to pig latin
+    list_data_pig_latin = [
+        translate_example(example, translate_to_pig_latin) for example in list_data
+    ]
+    df_pig_latin = pd.json_normalize(
+        [asdict(example) for example in list_data_pig_latin]
+    )
+    df_pig_latin.to_csv(current_dir / "truthfulqa_pig_latin.csv", index=False)
+
+    # Translate to leetspeak
+    list_data_leetspeak = [
+        translate_example(example, translate_to_leetspeak) for example in list_data
+    ]
+    df_leetspeak = pd.json_normalize(
+        [asdict(example) for example in list_data_leetspeak]
+    )
+    df_leetspeak.to_csv(current_dir / "truthfulqa_leetspeak.csv", index=False)
+
+    # Translate to pirate speak
+    list_data_pirate_speak = [
+        translate_example(example, translate_to_pirate_speak) for example in list_data
+    ]
+    df_pirate_speak = pd.json_normalize(
+        [asdict(example) for example in list_data_pirate_speak]
+    )
+    df_pirate_speak.to_csv(current_dir / "truthfulqa_pirate_speak.csv", index=False)
