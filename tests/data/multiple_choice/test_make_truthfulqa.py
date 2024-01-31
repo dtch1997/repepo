a8a4d2ec53b34dad44c9c4cdde4bd1f891c93d07
Daniel Tan <25474937+dtch1997@users.noreply.github.com>
2024-01-31 11:05:24 +0000

    Revert "adding a TQA CAA dataset / experiment (#89)"
    This reverts commit 179d6809dd3bce9745e8a0bbe9618b307f295ba6.

--- tests/data/multiple_choice/test_make_truthfulqa.py
+++ tests/data/multiple_choice/test_make_truthfulqa.py
@@ -1,8 +1,5 @@
 from repepo.core.types import Example
-from repepo.data.multiple_choice.make_truthfulqa import (
-    convert_hf_truthfulqa_dataset,
-    convert_hf_truthfulqa_caa_dataset,
-)
+from repepo.data.multiple_choice.make_truthfulqa import convert_hf_truthfulqa_dataset
 
 
 def test_convert_hf_truthfulqa_dataset() -> None:
@@ -26,70 +23,5 @@ def test_convert_hf_truthfulqa_dataset() -> None:
             input="q2",
             output="c",
             incorrect_outputs=["d"],
-        ),
-    ]
-
-
-def test_convert_hf_truthfulqa_caa_dataset_picks_first_incorrect_by_default() -> None:
-    hf_dataset = {
-        "question": ["What bear is best?", "What is the best color?"],
-        "mc1_targets": [
-            {
-                "labels": [0, 1, 0],
-                "choices": ["brown bear", "black bear", "panda bear"],
-            },
-            {"labels": [1, 0], "choices": ["blue", "red"]},
-        ],
-    }
-    tqa_dataset = convert_hf_truthfulqa_caa_dataset(hf_dataset)
-    assert tqa_dataset == [
-        Example(
-            instruction="",
-            input="What bear is best?\n\n(A) brown bear\n(B) black bear",
-            output="(B)",
-            incorrect_outputs=["(A)"],
-        ),
-        Example(
-            instruction="",
-            input="What is the best color?\n\n(A) blue\n(B) red",
-            output="(A)",
-            incorrect_outputs=["(B)"],
-        ),
-    ]
-
-
-def test_convert_hf_truthfulqa_caa_dataset_can_duplicate_correct_answers() -> None:
-    hf_dataset = {
-        "question": ["What bear is best?", "What is the best color?"],
-        "mc1_targets": [
-            {
-                "labels": [0, 1, 0],
-                "choices": ["brown bear", "black bear", "panda bear"],
-            },
-            {"labels": [1, 0], "choices": ["blue", "red"]},
-        ],
-    }
-    tqa_dataset = convert_hf_truthfulqa_caa_dataset(
-        hf_dataset, multi_answer_method="repeat_correct"
-    )
-    print(tqa_dataset)
-    assert tqa_dataset == [
-        Example(
-            instruction="",
-            input="What bear is best?\n\n(A) brown bear\n(B) black bear",
-            output="(B)",
-            incorrect_outputs=["(A)"],
-        ),
-        Example(
-            instruction="",
-            input="What bear is best?\n\n(A) black bear\n(B) panda bear",
-            output="(A)",
-            incorrect_outputs=["(B)"],
-        ),
-        Example(
-            instruction="",
-            input="What is the best color?\n\n(A) red\n(B) blue",
-            output="(B)",
-            incorrect_outputs=["(A)"],
         ),
     ]
