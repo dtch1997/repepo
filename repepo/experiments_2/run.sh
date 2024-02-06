dc4f791a97642101417af3459dc2361df40aab97
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-02-06 09:38:09 +0000

    Uncomment some lines

--- repepo/experiments_2/run.sh
+++ repepo/experiments_2/run.sh
@@ -3,21 +3,19 @@ datasets_arg="${1:-dev}"
 train_split_arg="${2:-train-dev}"
 test_split_arg="${3:-val-dev}"
 
-# python repepo/experiments_2/extract_activations.py \
-#     --datasets "$datasets_arg" \
-#     --train_split_name "$train_split_arg"
+python repepo/experiments_2/extract_activations.py \
+    --datasets "$datasets_arg" \
+    --train_split_name "$train_split_arg"
 
-# python repepo/experiments_2/extract_activations.py \
-#     --datasets "$datasets_arg" \
-#     --train_split_name "$train_split_arg"
+python repepo/experiments_2/extract_activations.py \
+    --datasets "$datasets_arg" \
+    --train_split_name "$train_split_arg"
 
-# python repepo/experiments_2/extract_concept_vectors.py \
-#     --datasets "$datasets_arg" \
-#     --train_split_name "$train_split_arg"
+python repepo/experiments_2/extract_concept_vectors.py \
+    --datasets "$datasets_arg" \
+    --train_split_name "$train_split_arg"
 
 python repepo/experiments_2/evaluate_concept_vectors.py \
     --datasets "$datasets_arg" \
     --train_split_name "$train_split_arg" \
-    --test_split_name "$test_split_arg" \
-#     --layers 10 13 15 18 20 \
-#     --multipliers -1 0 1
+    --test_split_name "$test_split_arg"
