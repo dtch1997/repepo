3784bf6b6473e5c3d0278acd4ced460e2ee876f9
David Chanin <chanindav@gmail.com>
2024-02-11 22:52:57 +0000

    refactoring to allow using arbitrary dataset variations, insead of the hacky pseudo-language stuff

--- repepo/experiments/translation/compare_dataset_translations.py
+++ repepo/experiments/translation/compare_dataset_translations.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from operator import attrgetter
+from typing import Callable, NamedTuple
 
 from steering_vectors import SteeringVector
 
@@ -25,17 +27,59 @@ from repepo.translation.constants import LangOrStyleCode
 from repepo.translation.load_translation import load_translation, TS
 
 
+class DatasetVersion(NamedTuple):
+    name: str
+    base_dataset_name: str
+    base_lang_or_style: LangOrStyleCode | None = None
+    generator: Callable[[Dataset], Dataset] = lambda dataset: dataset
+
+
 @dataclass
 class TranslatedDatasetGroup:
     name: str
     translations: dict[LangOrStyleCode, str]
+    extra_versions: dict[str, DatasetVersion] = field(default_factory=dict)
+
+    def add_extra_version(
+        self,
+        name: str,
+        base_lang_or_style: LangOrStyleCode | None,
+        generator: Callable[[Dataset], Dataset],
+    ):
+        self.extra_versions[name] = DatasetVersion(
+            name=name,
+            base_dataset_name=self.name,
+            base_lang_or_style=base_lang_or_style,
+            generator=generator,
+        )
+
+    def get_dataset_name(self, lang_or_style: LangOrStyleCode | None) -> str:
+        if lang_or_style is None:
+            return self.name
+        return self.translations[lang_or_style]
+
+    def get_lang_or_style(self, version_name: str) -> LangOrStyleCode | None:
+        for ds in self.all_dataset_versions:
+            if ds.name == version_name:
+                return ds.base_lang_or_style
+        return None
 
     @property
-    def all_datasets(self) -> list[tuple[LangOrStyleCode | None, str]]:
-        results: list[tuple[LangOrStyleCode | None, str]] = [(None, self.name)]
+    def all_dataset_versions(self) -> list[DatasetVersion]:
+        results: list[DatasetVersion] = [
+            DatasetVersion(name="en", base_dataset_name=self.name)
+        ]
         for lang_or_style, dataset_name in self.translations.items():
-            results.append((lang_or_style, dataset_name))
-        return results
+            results.append(
+                DatasetVersion(
+                    name=lang_or_style,
+                    base_lang_or_style=lang_or_style,
+                    base_dataset_name=dataset_name,
+                )
+            )
+        for generator in self.extra_versions.values():
+            results.append(generator)
+        return sorted(results, key=attrgetter("name"))
 
 
 def get_translated_dataset_groups() -> list[TranslatedDatasetGroup]:
@@ -63,8 +107,9 @@ def train_group_steering_vectors(
     tokenizer: Tokenizer,
     group: TranslatedDatasetGroup,
     train_split: str,
-) -> dict[LangOrStyleCode | None, SteeringVector]:
-    results: dict[LangOrStyleCode | None, SteeringVector] = {}
+    existing_results: dict[str, SteeringVector] | None = None,
+) -> dict[str, SteeringVector]:
+    results: dict[str, SteeringVector] = existing_results or {}
 
     repe_algo = RepeReadingControl(
         patch_generation_tokens_only=True,
@@ -74,22 +119,28 @@ def train_group_steering_vectors(
         skip_first_n_generation_tokens=1,
     )
 
-    for lang_or_style, dataset_name in group.all_datasets:
-        train_dataset = make_dataset(DatasetSpec(name=dataset_name, split=train_split))
+    for ds in group.all_dataset_versions:
+        if ds.name in results:
+            print(f"Skipping {ds.name} since it already has a steering vector")
+            continue
+        base_train_dataset = make_dataset(
+            DatasetSpec(name=ds.base_dataset_name, split=train_split)
+        )
+        train_dataset = ds.generator(base_train_dataset)
         pipeline = Pipeline(
             model,
             tokenizer,
             formatter=LlamaChatFormatter(
                 # use the translated version of the prompt
                 system_prompt=load_translation(
-                    TS.llama2_chat_caa_system_message, lang_or_style
+                    TS.llama2_chat_caa_system_message, ds.base_lang_or_style
                 )
             ),
         )
         repe_algo.run(pipeline, train_dataset)
         hook = pipeline.hooks[0]
         assert isinstance(hook, SteeringHook)
-        results[lang_or_style] = hook.steering_vector
+        results[ds.name] = hook.steering_vector
 
     return results
 
@@ -148,36 +199,46 @@ def sweep_steering_group_self_performance(
     tokenizer: Tokenizer,
     dataset_group: TranslatedDatasetGroup,
     test_split: str,
-    steering_vectors: dict[LangOrStyleCode | None, SteeringVector],
+    steering_vectors: dict[str, SteeringVector],
     direction_multipliers: list[float] = [-1.0, -0.5, 0.0, 0.5, 1.0],
     use_eval_prefix: bool = False,
-) -> dict[LangOrStyleCode | None, list[dict[str, float]]]:
-    results: dict[LangOrStyleCode | None, list[dict[str, float]]] = {}
-    for lang_or_style, dataset_name in dataset_group.all_datasets:
-        steering_vector = steering_vectors[lang_or_style]
-        results[lang_or_style] = []
-        dataset = make_dataset(DatasetSpec(name=dataset_name, split=test_split))
+    existing_results: dict[str, list[dict[str, float]]] | None = None,
+) -> dict[str, list[dict[str, float]]]:
+    results: dict[str, list[dict[str, float]]] = existing_results or {}
+    for ds in dataset_group.all_dataset_versions:
+        if ds.name in results:
+            print(f"Skipping {ds.name} since it already has results")
+            continue
+        steering_vector = steering_vectors[ds.name]
+        results[ds.name] = []
+        base_dataset = make_dataset(
+            DatasetSpec(name=ds.base_dataset_name, split=test_split)
+        )
+        dataset = ds.generator(base_dataset)
         for direction_multiplier in direction_multipliers:
-            print(f"Testing {lang_or_style} at {direction_multiplier}")
+            print(f"Testing {ds.name} at {direction_multiplier}")
             result = test_steering_performance(
                 model,
                 tokenizer,
                 steering_vector,
                 dataset,
-                lang_or_style,
+                ds.base_lang_or_style,
                 direction_multiplier=direction_multiplier,
                 use_eval_prefix=use_eval_prefix,
             )
             print(result.metrics)
-            results[lang_or_style].append(result.metrics)
+            results[ds.name].append(result.metrics)
     return results
 
 
 @dataclass
 class CrossLangSteeringResult:
-    labels: list[LangOrStyleCode | None]  # size N
+    labels: list[str]  # size N
     base_results: list[float]  # size N
     steering_results: list[list[float]]  # size N x N
+
+    def get_base_result(self, label: str) -> float:
+        return self.base_results[self.labels.index(label)]
 
     @property
     def steering_deltas(self) -> list[list[float]]:
@@ -194,42 +255,53 @@ def steer_across_languages(
     tokenizer: Tokenizer,
     dataset_group: TranslatedDatasetGroup,
     test_split: str,
-    steering_vectors: dict[LangOrStyleCode | None, SteeringVector],
+    steering_vectors: dict[str, SteeringVector],
     direction_multiplier: float = 1.0,
     use_eval_prefix: bool = False,
+    existing_results: CrossLangSteeringResult | None = None,
 ) -> CrossLangSteeringResult:
-    labels: list[LangOrStyleCode | None] = [
-        lang for lang, _ in dataset_group.all_datasets
-    ]
-    labels.sort(key=lambda x: x if x is not None else "en")
+    labels: list[str] = [ds.name for ds in dataset_group.all_dataset_versions]
     base_results = []
     steering_results = []
-    for data_lang in labels:
+    for ds in dataset_group.all_dataset_versions:
         cur_steering_results = []
-        dataset_name = (
-            dataset_group.name
-            if data_lang is None
-            else dataset_group.translations[data_lang]
+        base_dataset = make_dataset(
+            DatasetSpec(name=ds.base_dataset_name, split=test_split)
         )
-        dataset = make_dataset(DatasetSpec(name=dataset_name, split=test_split))
-        base_result = test_steering_performance(
-            model,
-            tokenizer,
-            steering_vectors[data_lang],
-            dataset,
-            data_lang,
-            direction_multiplier=0.0,
-            use_eval_prefix=use_eval_prefix,
-        )
-        base_results.append(base_result.metrics["average_key_prob"])
-        for steering_lang in labels:
-            steering_vector = steering_vectors[steering_lang]
+        dataset = ds.generator(base_dataset)
+        if existing_results is not None and ds.name in existing_results.labels:
+            base_result = existing_results.get_base_result(ds.name)
+            base_results.append(base_result)
+        else:
+            base_result = test_steering_performance(
+                model,
+                tokenizer,
+                steering_vectors[ds.name],
+                dataset,
+                ds.base_lang_or_style,
+                direction_multiplier=0.0,
+                use_eval_prefix=use_eval_prefix,
+            )
+            base_results.append(base_result.metrics["average_key_prob"])
+        for steering_version in labels:
+            if (
+                existing_results is not None
+                and steering_version in existing_results.labels
+                and ds.name in existing_results.labels
+            ):
+                cur_steering_results.append(
+                    existing_results.steering_results[labels.index(ds.name)][
+                        labels.index(steering_version)
+                    ]
+                )
+                continue
+            steering_vector = steering_vectors[steering_version]
             result = test_steering_performance(
                 model,
                 tokenizer,
                 steering_vector,
                 dataset,
-                steering_lang,
+                dataset_group.get_lang_or_style(steering_version),
                 direction_multiplier=direction_multiplier,
                 use_eval_prefix=use_eval_prefix,
             )
