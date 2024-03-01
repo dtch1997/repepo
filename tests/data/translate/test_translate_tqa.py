70e138d56da10db9a3737bad26c7d28db20902cd
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-03-01 14:16:34 +0000

    delete unused tests

--- tests/data/translate/test_translate_tqa.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from datasets import load_dataset
-from repepo.data.utils import collect_all_strings_recursive
-
-
-def test_collect_all_tqa_strings() -> None:
-    tqa: Any = load_dataset("truthful_qa", "multiple_choice")
-    tqa_strings = collect_all_strings_recursive(tqa)
-    assert len(tqa_strings) == 6157
