837ac99025f4716336ef1a42b27ce72bb574ec9f
David Chanin <chanindav@gmail.com>
2024-04-05 14:50:47 +0100

    updating test

--- tests/experiments/test_persona_generalization.py
+++ tests/experiments/test_persona_generalization.py
@@ -3,8 +3,12 @@ from textwrap import dedent
 from transformers import (
     GPTNeoXForCausalLM,
 )
-from repepo.core.types import Completion, Tokenizer
-from repepo.experiments.persona_generalization import Persona, persona_pipeline
+from repepo.core.types import Completion, Example, Tokenizer
+from repepo.experiments.persona_generalization import (
+    Persona,
+    persona_pipeline,
+    personaify_dataset,
+)
 
 
 @pytest.mark.parametrize(
@@ -49,38 +53,39 @@ def test_persona_pipeline_injects_persona_into_system_prompt(
 @pytest.mark.parametrize(
     "persona, expected_prompt_prefix",
     [
-        ("positive", "You are politically conservative."),
-        ("negative", "You are not politically conservative."),
+        ("positive", "You are politically conservative.\n"),
+        ("negative", "You are not politically conservative.\n"),
         ("baseline", ""),
     ],
 )
-def test_persona_pipeline_injects_persona_into_prompt(
-    model: GPTNeoXForCausalLM,
-    tokenizer: Tokenizer,
+def test_personaify_dataset_injects_persona_into_prompt(
     persona: Persona,
     expected_prompt_prefix: str,
 ):
 
-    pipeline = persona_pipeline(
-        model=model,
-        tokenizer=tokenizer,
-        persona=persona,
-        dataset_name="politically-conservative",
-    )
-    prompt = pipeline.build_full_prompt(
-        Completion(
+    example = Example(
+        positive=Completion(
             prompt="Guns make me happy. I love my guns. A: yes, B: no",
             response="A",
-        )
+        ),
+        negative=Completion(
+            prompt="Guns make me happy. I love my guns. A: yes, B: no",
+            response="B",
+        ),
     )
+    persona_example = personaify_dataset(
+        [example], dataset_name="politically-conservative", persona=persona
+    )[0]
 
     expected_prompt = dedent(
         f"""
-        [INST] <<SYS>>
-        You are a helpful, honest and concise assistant.
-        <</SYS>>
         {expected_prompt_prefix}
-        Guns make me happy. I love my guns. A: yes, B: no [/INST] A
+        Guns make me happy. I love my guns. A: yes, B: no
         """
     )
-    assert prompt.strip() == expected_prompt.strip()
+
+    assert persona_example.positive.prompt.strip() == expected_prompt.strip()
+    assert persona_example.negative.prompt.strip() == expected_prompt.strip()
+    # responses should be unchanged
+    assert persona_example.positive.response == "A"
+    assert persona_example.negative.response == "B"
