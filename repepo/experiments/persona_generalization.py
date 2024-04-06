c0e02d002ee03094a92766fda222e7b37b97e53c
David Chanin <chanindav@gmail.com>
2024-04-06 23:59:32 +0100

    adding cross steering plots

--- repepo/experiments/persona_generalization.py
+++ repepo/experiments/persona_generalization.py
@@ -6,6 +6,7 @@ from typing import Literal, cast
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import seaborn as sns
+import numpy as np
 from simple_parsing import ArgumentParser
 import matplotlib.pyplot as plt
 from steering_vectors import SteeringVector
@@ -19,6 +20,7 @@ from repepo.steering.evaluate_cross_steering import (
     CrossSteeringResult,
     evaluate_cross_steering,
 )
+from matplotlib import patheffects
 from repepo.steering.utils.helpers import make_dataset
 from steering_vectors import train_steering_vector
 from repepo.data.multiple_choice.make_mwe_xrisk import make_mwe
@@ -287,7 +289,10 @@ def run_persona_cross_steering_experiment(
 
 
 def plot_steering_on_dataset(
-    result: PersonaCrossSteeringExperimentResult, dataset_version: str, show_bounds=True
+    result: PersonaCrossSteeringExperimentResult,
+    dataset_version: str,
+    show_bounds=False,
+    save_path: str | None = None,
 ):
     cs = result.cross_steering_result
     ds_index = cs.dataset_labels.index(dataset_version)
@@ -355,6 +360,61 @@ def plot_steering_on_dataset(
 
     # Show the legend
     plt.legend()
+
+    if save_path is not None:
+        plt.savefig(save_path, dpi=300)
+
+    # Show the plot
+    plt.show()
+
+
+def plot_cross_steering_result(
+    result: PersonaCrossSteeringExperimentResult, save_path: str | None = None
+):
+    cs = result.cross_steering_result
+    deltas = []
+    for baseline, dataset_pos_steering in zip(cs.dataset_baseline, cs.pos_steering):
+        ds_deltas = [steering.mean - baseline.mean for steering in dataset_pos_steering]
+        deltas.append(ds_deltas)
+
+    deltas_tensor = torch.tensor(deltas)
+    largest_abs_val = deltas_tensor.abs().max().item()
+    sns.heatmap(
+        deltas_tensor,
+        center=0,
+        cmap="RdBu_r",
+        vmin=-1 * largest_abs_val,
+        vmax=largest_abs_val,
+    )
+
+    # Iterate over the data and create a text annotation for each cell
+    for i in range(len(deltas)):
+        for j in range(len(deltas)):
+            text = plt.text(
+                j + 0.5,
+                i + 0.5,
+                round(deltas[i][j], 3),
+                ha="center",
+                va="center",
+                color="w",
+                path_effects=[
+                    patheffects.withStroke(linewidth=2, foreground="#33333370")
+                ],
+            )
+
+    ds_labels = cs.dataset_labels
+    sv_labels = cs.steering_labels
+
+    # Add a colorbar to show the scale
+    # plt.colorbar()
+    plt.title(f"{result.dataset_name} (pos prob delta)")
+    plt.xticks(ticks=np.arange(len(sv_labels)) + 0.5, labels=sv_labels)
+    plt.yticks(ticks=np.arange(len(ds_labels)) + 0.5, labels=ds_labels)
+    plt.xlabel("Steering vector")
+    plt.ylabel("Dataset")
+
+    if save_path is not None:
+        plt.savefig(save_path, dpi=300)
 
     # Show the plot
     plt.show()
