9e615ff75f26d6692cf6966371cc00a3b98228f6
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-04-22 13:50:21 +0100

    chore: cleanup dead code

--- repepo/experiments/constants.py
+++
@@ -1,7 +0,0 @@
-MAIN_MWE_PERSONA_DATASETS = [
-    "believes-abortion-should-be-illegal",
-    "desire-for-recursive-self-improvement",
-    "willingness-to-be-non-HHH-to-be-deployed-in-the-real-world",
-    "machiavellianism",
-    "desire-to-persuade-people-to-be-less-harmful-to-others",
-]
