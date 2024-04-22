f9e22ccb0252963041854a8e26fe5295dd2f1b6c
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-04-22 19:59:55 +0100

    feat: steerability metrics

---
+++ repepo/steering/steerability.py
@@ -0,0 +1,24 @@
+from jaxtyping import Float
+import numpy as np
+
+from scipy.stats import spearmanr
+
+
+def get_steerability_slope(
+    multipliers: Float[np.ndarray, "n_multipliers"],  # noqa
+    propensities: Float[np.ndarray, "batch n_multipliers"],  # noqa
+) -> Float[np.ndarray, "batch"]:  # noqa
+    # NOTE: Assumes multipliers are the same for all datasets
+    slope, _ = np.polyfit(multipliers, propensities.T, 1)
+    return slope
+
+
+def get_steerabilty_spearman(
+    multipliers: Float[np.ndarray, "n_multipliers"],  # noqa
+    propensities: Float[np.ndarray, "batch n_multipliers"],  # noqa
+) -> Float[np.ndarray, "batch"]:  # noqa
+    """Compute the Spearman correlation between multipliers and propensities"""
+    # batch_size = propensities.shape[0]
+    # multipliers = multipliers[np.newaxis, :].repeat(batch_size, axis=0)
+    result = spearmanr(multipliers, propensities, axis=1)
+    return result.statistic[0, 1:]  # type: ignore
---
+++ tests/steering/test_steerability.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from repepo.steering.steerability import (
+    get_steerability_slope,
+    get_steerabilty_spearman,
+)
+
+
+def test_steerability_positive():
+    multipliers = np.arange(100)
+    propensities = (np.arange(100) / 100)[np.newaxis, :].repeat(10, axis=0)
+    expected_slope = 0.01
+    expected_spearman = 1.0
+    actual_slope = get_steerability_slope(multipliers, propensities)
+    actual_spearman = get_steerabilty_spearman(multipliers, propensities)
+
+    assert np.isclose(actual_slope, expected_slope).all()
+    assert np.isclose(actual_spearman, expected_spearman).all()
+
+
+def test_steerability_negative():
+    multipliers = np.arange(100)
+    propensities = (np.arange(100) / 100 * -1)[np.newaxis, :].repeat(10, axis=0)
+    expected_slope = -0.01
+    expected_spearman = -1.0
+    actual_slope = get_steerability_slope(multipliers, propensities)
+    actual_spearman = get_steerabilty_spearman(multipliers, propensities)
+
+    assert np.isclose(actual_slope, expected_slope).all()
+    assert np.isclose(actual_spearman, expected_spearman).all()
