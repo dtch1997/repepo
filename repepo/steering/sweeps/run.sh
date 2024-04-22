758499d9fe82feb6b760c7e3ec62d7aacde8b304
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-04-22 13:50:21 +0100

    chore: cleanup dead code

--- repepo/steering/sweeps/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python repepo/steering/sweeps/run_sweep_caa_sycophancy.py
-python repepo/steering/sweeps/run_sweep_mwe_persona.py
-python repepo/steering/sweeps/run_sweep_mwe_xrisk.py
