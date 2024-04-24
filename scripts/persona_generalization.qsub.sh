d1268c8dff7932f2b54c7f6b858663a6bf4f057e
dtch1997 <dtch1997@users.noreply.github.com>
2024-04-24 16:42:52 +0100

    chore: update time requirement in qsub

--- scripts/persona_generalization.qsub.sh
+++ scripts/persona_generalization.qsub.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 #$ -N persona_generalization             # Specify the job name
+#$ -l h_rt=72:00:00
 #$ -l h_vmem=64G           # Request 64GB of memory per job
 #$ -l gpu=1               # Request 1 GPU
 #$ -ac allow=L        # Specify the type of GPU
-
-# Load the required modules
-module load python3/3.11  # Load Python 3.11 module
-module load cuda/11.2     # Load CUDA module, adjust version as necessary
 
 # NOTE: Assume you have already cloned the repository and checked out correct branch
 # Navigate to the project directory
 cd /home/$USER/Scratch/repepo
 
-# Create a Python virtual environment
-python3 -m venv .venv
-
-# Activate the virtual environment
-source .venv/bin/activate
-
-# Install dependencies
-pip install -e . 
+# NOTE: Assume you have already installed pdm
+pdm install
 
 # Run the script
-python repepo/experiments/persona_generalization.py
-
-# Deactivate the virtual environment
-deactivate
+pdm run python repepo/experiments/persona_generalization.py --output_dir repepo/experiments/persona_generalization
