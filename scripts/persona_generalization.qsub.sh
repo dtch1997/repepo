dcd731a079ea48d587f5db8736d581f11caeb8fd
dtch1997 <dtch1997@users.noreply.github.com>
2024-04-25 11:02:04 +0100

    chore: add cluster utils

--- scripts/persona_generalization.qsub.sh
+++ scripts/persona_generalization.qsub.sh
@@ -4,17 +4,21 @@
 #$ -l h_vmem=64G           # Request 64GB of memory per job
 #$ -l gpu=1               # Request 1 GPU
 #$ -ac allow=L        # Specify the type of GPU
+#$ -o $HOME/logs
+#$ -e $HOME/logs
 
 # Add locally installed executables to PATH
-#  (e.g. pdm)
-export PATH=$PATH:/home/$USER/.local/bin
+source /home/$USER/.bash_profile
+send_slack_notification "Job $JOB_NAME:$JOB_ID started"
 
 # NOTE: Assume you have already cloned the repository and checked out correct branch
 # Navigate to the project directory
 cd /home/$USER/Scratch/repepo
+
 
 # NOTE: Assume you have already installed pdm
 pdm install
 
 # Run the script
 pdm run python repepo/experiments/persona_generalization.py --output_dir repepo/experiments/persona_generalization
+send_slack_notification "Job $JOB_NAME:$JOB_ID ended"
