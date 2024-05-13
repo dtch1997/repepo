0555478ed8c32a8dc1e5fbb4aa481819202c6ad0
David Chanin <chanindav@gmail.com>
2024-05-13 15:07:44 +0100

    tweaking qwen script

--- scripts/persona_generalization_qwen.qsub.sh
+++ scripts/persona_generalization_qwen.qsub.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 #$ -N persona_generalization_qwen            # Specify the job name
 #$ -l h_rt=72:00:00
 #$ -l h_vmem=64G           # Request 64GB of memory per job
 #$ -l gpu=1               # Request 1 GPU
 #$ -ac allow=L        # Specify the type of GPU
-#$ -o $HOME/logs
-#$ -e $HOME/logs
+#$ -S /bin/bash
+#$ -j y
 #$ -t 1-40
 
 nvidia-smi
