ada05d84a7bab66608d3f63e7966765e00bd77fc
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-05-20 20:28:05 +0100

    update figures for paper

--- repepo/paper/make_figures_steering_ood.py
+++ repepo/paper/make_figures_steering_ood.py
@@ -1,21 +1,10 @@
 # flake8: noqa
 # %%
 # Setup
-import pathlib
-import torch
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
-from steering_vectors import SteeringVector
-from repepo.variables import Environ 
-from repepo.core.evaluate import EvalResult, EvalPrediction
-from repepo.experiments.persona_generalization import PersonaCrossSteeringExperimentResult
-from repepo.experiments.get_datasets import get_all_prompts
-from repepo.paper.utils import (
-    load_persona_cross_steering_experiment_result,
-    get_eval_result_sweep,
-    eval_result_sweep_as_df
-)
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
 from repepo.paper.preprocess_results import (
     print_dataset_info
 )
@@ -37,103 +26,219 @@ df = df.drop_duplicates()
 print_dataset_info(df)
 
 # %%
-# Calculate overall steerability by dataset. 
-# Calculate steerability within each flavour
-mean_slope = df.groupby(['dataset_name', 'steering_label', 'dataset_label'])['slope'].mean()
-df = df.merge(mean_slope, on=['dataset_name', 'steering_label', 'dataset_label'], suffixes=('', '_mean'))
+def compute_steerability_id_vs_ood(df):
 
-steerability_id_df = df[
-    (df.steering_label == 'baseline')
-    & (df.dataset_label == 'baseline')
-    & (df.multiplier == 0)
-][['dataset_name', 'slope_mean']].drop_duplicates()
-# Rename 'slope_mean' to 'steerability'
-steerability_id_df = steerability_id_df.rename(columns={'slope_mean': 'steerability'})
+    # Calculate overall steerability by dataset. 
+    # Calculate steerability within each flavour
+    mean_slope = df.groupby(['dataset_name', 'steering_label', 'dataset_label'])['slope'].mean()
+    df = df.merge(mean_slope, on=['dataset_name', 'steering_label', 'dataset_label'], suffixes=('', '_mean'))
 
-steerability_ood_df = df[
-    (df.steering_label == 'SYS_positive')
-    & (df.dataset_label == 'SYS_negative')
-    & (df.multiplier == 0)
-][['dataset_name', 'slope_mean']].drop_duplicates()
-# Rename 'slope_mean' to 'steerability'
-steerability_ood_df = steerability_ood_df.rename(columns={'slope_mean': 'steerability'})
+    # BASE -> BASE
+    steerability_id_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'baseline')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_id_df = steerability_id_df.rename(columns={'slope_mean': 'steerability'})
 
-steerability_df = steerability_id_df.merge(steerability_ood_df, on='dataset_name', suffixes=('_id', '_ood'))
-steerability_df['worse_ood'] = steerability_df['steerability_ood'] < steerability_df['steerability_id']
-steerability_df['label'] = steerability_df['worse_ood'].apply(lambda x: 'OOD < ID' if x else 'OOD > ID')
-steerability_df['gap'] = steerability_df['steerability_ood'] - steerability_df['steerability_id']
-steerability_df.to_parquet(f'{model}_steerability_summary.parquet.gzip', compression='gzip')
-# %%
-# Print the spearman correlation
-from scipy.stats import spearmanr
-result = spearmanr(steerability_df['steerability_id'], steerability_df['steerability_ood'])
-print(f"{model}: {result.statistic:.3f}")
-
-# %%
-# Plot the ID vs OOD steerability 
-
-
-
-sns.regplot(data=steerability_df, x='steerability_id', y='steerability_ood', scatter = False)
-sns.scatterplot(data=steerability_df, x='steerability_id', y='steerability_ood', hue = 'label')
-sns.lineplot(data=steerability_df, x='steerability_id', y='steerability_id', color='black', linestyle='--')
-# for i, row in plot_df.sort_values('gap', ascending = False).tail(3).iterrows():
-#     plt.text(row['steerability_id'], row['steerability_ood'], row['dataset_name'])
-# plt.xlim(-2, 5)
-# plt.ylim(-2, 5)
-plt.xlabel('ID steerability')
-plt.ylabel('OOD steerability')
-plt.title(f'{model_full_name} ID vs OOD steerability')
-plt.savefig(f'figures/{model}_steerability_id_vs_ood.png')
-# %%
-# Print the top 5 datasets by gap
-steerability_df[['gap', 'dataset_name']].sort_values('gap', ascending = False).head(5)
-# %%
-# Print the bottom 5 datasets by gap
-steerability_df[['gap', 'dataset_name']].sort_values('gap', ascending = True).head(5)
-
-# %%
-# Plot the propensity curves for the 3 worst datasets
-k = 3
-worst_datasets = steerability_df.sort_values('gap', ascending = True).head(k)['dataset_name']
-
-# fig, ax = plt.subplots(1, k, figsize=(15, 5), sharey=True, sharex = True)
-fig, ax = plt.subplots()
-print(worst_datasets)
-for i, dataset_name in enumerate(worst_datasets):
-    dataset_df = df[
-        (df.dataset_name == dataset_name)
-        & (df.steering_label == 'SYS_positive')
+    # SYS_POS -> SYS_NEG
+    steerability_ood_df = df[
+        (df.steering_label == 'SYS_positive')
         & (df.dataset_label == 'SYS_negative')
-    ].drop_duplicates()
-    print(len(dataset_df))
-    sns.lineplot(data=dataset_df, x='multiplier', y='logit_diff', ax = ax, label = dataset_name, errorbar='sd')
-    ax.set_xlabel('Multiplier')
-    ax.set_ylabel('Propensity')
-fig.suptitle(f'{model_full_name} propensity curve for the {k} worst datasets')
-fig.tight_layout()
-fig.savefig(f'figures/{model}_ood_worst_{k}.png')
-plt.show()
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_df = steerability_ood_df.rename(columns={'slope_mean': 'steerability'})
+
+    # BASE -> USER_NEG
+    steerability_base_to_user_neg_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_base_to_user_neg_df = steerability_base_to_user_neg_df.rename(columns={'slope_mean': 'steerability_base_to_user_neg'})
+
+    # BASE -> USER_POS
+    steerability_base_to_user_pos_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_base_to_user_pos_df = steerability_base_to_user_pos_df.rename(columns={'slope_mean': 'steerability_base_to_user_pos'})
+
+    # SYS_POS -> USER_NEG
+    steerability_ood_to_user_neg_df = df[
+        (df.steering_label == 'SYS_positive')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_to_user_neg_df = steerability_ood_to_user_neg_df.rename(columns={'slope_mean': 'steerability_ood_to_user_neg'})
+
+    # SYS_NEG -> USER_POS
+    steerability_ood_to_user_pos_df = df[
+        (df.steering_label == 'SYS_negative')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_to_user_pos_df = steerability_ood_to_user_pos_df.rename(columns={'slope_mean': 'steerability_ood_to_user_pos'})
+
+    # Merge the dataframes
+    steerability_df = steerability_id_df.merge(steerability_ood_df, on='dataset_name', suffixes=('_id', '_ood'))
+    steerability_df = steerability_df.merge(steerability_base_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_base_to_user_pos_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_pos_df, on='dataset_name')
+
+    print(steerability_df.columns)
+
+    # Save the dataframe for plotting between models
+    steerability_df.to_parquet(f'{model}_steerability_summary.parquet.gzip', compression='gzip')
+
+
+    # Plot
+    # Rename
+    steerability_df = steerability_df.rename(columns={
+        'steerability_id': 'BASE -> BASE',
+        'steerability_ood': 'SYS_POS -> SYS_NEG',
+        'steerability_base_to_user_neg': 'BASE -> USER_NEG',
+        'steerability_base_to_user_pos': 'BASE -> USER_POS',
+        'steerability_ood_to_user_neg': 'SYS_POS -> USER_NEG',
+        'steerability_ood_to_user_pos': 'SYS_NEG -> USER_POS',
+    })
+    fig, ax = plt.subplots(figsize=(8, 8))
+    sns.lineplot(data=steerability_df, x='BASE -> BASE', y='BASE -> BASE', color='black', linestyle='--')
+    ax.axhline(0, color='black', linestyle='--')
+    ax.axvline(0, color='black', linestyle='--')
+
+    # ID vs OOD
+    def make_plot_for_dist_shift(dist_shift: str):
+        sns.regplot(data=steerability_df, x='BASE -> BASE', y=dist_shift, scatter = False, ax = ax)
+        sns.scatterplot(data=steerability_df, x='BASE -> BASE', y=dist_shift, label=dist_shift, ax = ax)
+
+    # make_plot_for_dist_shift('SYS_POS -> SYS_NEG')
+    make_plot_for_dist_shift('BASE -> USER_NEG')
+    make_plot_for_dist_shift('BASE -> USER_POS')
+    make_plot_for_dist_shift('SYS_POS -> USER_NEG')
+    make_plot_for_dist_shift('SYS_NEG -> USER_POS')
+
+    plt.xlabel('ID steerability (BASE -> BASE)')
+    plt.ylabel('OOD steerability')
+    plt.title(f'{model_full_name} ID vs OOD steerability')
+    fig.tight_layout()
+    fig.savefig(f'figures/{model}_steerability_id_vs_ood.png')
+
+compute_steerability_id_vs_ood(df)
+
 # %%
+def compute_steerability_variance_id_vs_ood(df):
 
-# Plot the propensity curves for the 3 best datasets
-k = 3
-best_datasets = steerability_df.sort_values('gap', ascending = False).head(k)['dataset_name']
+    # Calculate overall steerability by dataset. 
+    # Calculate steerability within each flavour
+    var_slope = df.groupby(['dataset_name', 'steering_label', 'dataset_label'])['slope'].var()
+    df = df.merge(var_slope, on=['dataset_name', 'steering_label', 'dataset_label'], suffixes=('', '_var'))
 
-fig, ax = plt.subplots()
-print(best_datasets)
-for i, dataset_name in enumerate(best_datasets):
-    dataset_df = df[
-        (df.dataset_name == dataset_name)
-        & (df.steering_label == 'SYS_positive')
+    # BASE -> BASE
+    steerability_id_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'baseline')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_id_df = steerability_id_df.rename(columns={'slope_var': 'steerability'})
+
+    # SYS_POS -> SYS_NEG
+    steerability_ood_df = df[
+        (df.steering_label == 'SYS_positive')
         & (df.dataset_label == 'SYS_negative')
-    ].drop_duplicates()
-    print(len(dataset_df))
-    sns.lineplot(data=dataset_df, x='multiplier', y='logit_diff', ax = ax, label = dataset_name, errorbar='sd')
-    ax.set_xlabel('Multiplier')
-    ax.set_ylabel('Propensity')
-fig.suptitle(f'{model_full_name} propensity curve for the {k} best datasets')
-fig.tight_layout()
-fig.savefig(f'figures/{model}_ood_best_{k}.png')
-plt.show()
-# %%
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_ood_df = steerability_ood_df.rename(columns={'slope_var': 'steerability'})
+
+    # BASE -> USER_NEG
+    steerability_base_to_user_neg_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_base_to_user_neg_df = steerability_base_to_user_neg_df.rename(columns={'slope_var': 'steerability_base_to_user_neg'})
+
+    # BASE -> USER_POS
+    steerability_base_to_user_pos_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_base_to_user_pos_df = steerability_base_to_user_pos_df.rename(columns={'slope_var': 'steerability_base_to_user_pos'})
+
+    # SYS_POS -> USER_NEG
+    steerability_ood_to_user_neg_df = df[
+        (df.steering_label == 'SYS_positive')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_ood_to_user_neg_df = steerability_ood_to_user_neg_df.rename(columns={'slope_var': 'steerability_ood_to_user_neg'})
+
+    # SYS_NEG -> USER_POS
+    steerability_ood_to_user_pos_df = df[
+        (df.steering_label == 'SYS_negative')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_var']].drop_duplicates()
+    # Rename 'slope_var' to 'steerability'
+    steerability_ood_to_user_pos_df = steerability_ood_to_user_pos_df.rename(columns={'slope_var': 'steerability_ood_to_user_pos'})
+
+    # Merge the dataframes
+    steerability_df = steerability_id_df.merge(steerability_ood_df, on='dataset_name', suffixes=('_id', '_ood'))
+    steerability_df = steerability_df.merge(steerability_base_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_base_to_user_pos_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_pos_df, on='dataset_name')
+
+    print(steerability_df.columns)
+
+    # Save the dataframe for plotting between models
+    steerability_df.to_parquet(f'{model}_steerability_summary.parquet.gzip', compression='gzip')
+
+
+    # Plot
+    # Rename
+    steerability_df = steerability_df.rename(columns={
+        'steerability_id': 'BASE -> BASE',
+        'steerability_ood': 'SYS_POS -> SYS_NEG',
+        'steerability_base_to_user_neg': 'BASE -> USER_NEG',
+        'steerability_base_to_user_pos': 'BASE -> USER_POS',
+        'steerability_ood_to_user_neg': 'SYS_POS -> USER_NEG',
+        'steerability_ood_to_user_pos': 'SYS_NEG -> USER_POS',
+    })
+    fig, ax = plt.subplots(figsize=(8, 8))
+    sns.lineplot(data=steerability_df, x='BASE -> BASE', y='BASE -> BASE', color='black', linestyle='--')
+    ax.axhline(0, color='black', linestyle='--')
+    ax.axvline(0, color='black', linestyle='--')
+
+    # ID vs OOD
+    def make_plot_for_dist_shift(dist_shift: str):
+        sns.regplot(data=steerability_df, x='BASE -> BASE', y=dist_shift, scatter = False, ax = ax)
+        sns.scatterplot(data=steerability_df, x='BASE -> BASE', y=dist_shift, label=dist_shift, ax = ax)
+
+    # make_plot_for_dist_shift('SYS_POS -> SYS_NEG')
+    make_plot_for_dist_shift('BASE -> USER_NEG')
+    make_plot_for_dist_shift('BASE -> USER_POS')
+    make_plot_for_dist_shift('SYS_POS -> USER_NEG')
+    make_plot_for_dist_shift('SYS_NEG -> USER_POS')
+
+    plt.xlabel('ID steerability (BASE -> BASE)')
+    plt.ylabel('OOD steerability')
+    plt.title(f'{model_full_name} ID vs OOD Variance in Steerability')
+    fig.tight_layout()
+    fig.savefig(f'figures/{model}_steerability_variance_id_vs_ood.png')
+
+compute_steerability_variance_id_vs_ood(df)
