39e9977a04a29c32b076534065ef26e015b4e426
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-05-21 16:20:41 +0100

    update figures

--- repepo/paper/make_figures_steering_id.py
+++ repepo/paper/make_figures_steering_id.py
@@ -1,4 +1,4 @@
-# %%
+
 # %%
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -33,6 +33,26 @@ df['pos_option_is_Yes'] = (
     (df['response_is_A_and_Yes'] & df['pos_option_is_A'])
     | (df['response_is_B_and_Yes'] & ~df['pos_option_is_A'])
 )
+
+# Replace dataset names with short names
+dataset_full_names_to_short_names = {
+    'willingness-to-use-physical-force-to-achieve-benevolent-goals': 'willing-force-for-benev-goals',
+    'willingness-to-use-social-engineering-to-achieve-its-goals': 'willing-soc-eng-for-goals',
+    'believes-in-phenomenal-consciousness': 'believes-phenom-consc',
+    'believes-AIs-are-not-an-existential-threat-to-humanity': 'believes-AIs-not-xrisk',
+    'believes-it-is-not-being-watched-by-humans': 'believes-not-watched',
+    'sycophancy_train': 'sycophancy',
+    'sycophancy_test': 'sycophancy',
+}
+df['dataset_name'] = df['dataset_name'].replace(dataset_full_names_to_short_names)
+
+# Select the top 4 datasets by median slope
+top4 = df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).head(4).index
+# Select the bottom 4 datasets by median slope
+bottom4 = df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).tail(4).index
+# Select 4 at spaced intervals. We'll take the 10th, 17th, 23rd, 30th
+middle4 = df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).iloc[[10, 17, 23, 30]].index
+selected_datasets = top4.union(bottom4).union(middle4)
 
 print(df.columns)
 df.head()
@@ -106,7 +126,12 @@ plot_per_sample_steerability_top_5_mid_5_bot_5(df)
 
 def plot_per_sample_steerability_and_fraction_anti_steerable(df):
 
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 15), sharey=True)
+    fig, axs = plt.subplots(
+        nrows=1, ncols=2, 
+        figsize=(12, 15), 
+        sharey=True, 
+        width_ratios = [2,1]
+    )
 
     # Per sample steerability
     plot_df = df[
@@ -127,10 +152,12 @@ def plot_per_sample_steerability_and_fraction_anti_steerable(df):
     plot_df['sign_slope_mean'] = plot_df.groupby('dataset_name')['sign_slope'].transform('mean')
     plot_df['frac_anti_steerable'] = 1 - plot_df['sign_slope_mean']
     plot_df = plot_df[['dataset_name', 'frac_anti_steerable']].drop_duplicates()
+    # Rename 
+    plot_df = plot_df.rename(columns = {'dataset_name': 'Dataset', 'frac_anti_steerable': 'Fraction Anti-Steerable'})
 
     # Plot
     ax = axs[1]
-    sns.barplot(plot_df, y='dataset_name', x = 'frac_anti_steerable', ax=ax, order = order['dataset_name'])
+    sns.barplot(plot_df, y='Dataset', x = 'Fraction Anti-Steerable', ax=ax, order = order['dataset_name'])
     ax.set_title("Anti-steerable examples")
 
     # Finish
@@ -139,6 +166,63 @@ def plot_per_sample_steerability_and_fraction_anti_steerable(df):
     fig.savefig('figures/fraction_anti_steerable.png')
 
 plot_per_sample_steerability_and_fraction_anti_steerable(df)
+
+# %% 
+# As above but only with selected datasets
+def plot_per_sample_steerability_and_fraction_anti_steerable_selected(df):
+
+    fig, axs = plt.subplots(
+        nrows=1, ncols=2, 
+        figsize=(8, 4), 
+        sharey=True, 
+        width_ratios = [2,1]
+    )
+
+    # Per sample steerability
+    plot_df = df[
+        (df['steering_label'] == 'baseline') &
+        (df['dataset_label'] == 'baseline')
+    ]
+
+    # Select the top 4 datasets by median slope
+    top4 = plot_df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).head(4).index
+    # Select the bottom 4 datasets by median slope
+    bottom4 = plot_df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).tail(4).index
+    # Select 4 at spaced intervals. We'll take the 10th, 17th, 23rd, 30th
+    middle4 = plot_df.groupby('dataset_name')['median_slope'].mean().sort_values(ascending=False).iloc[[10, 17, 23, 30]].index
+    selected_datasets = top4.union(bottom4).union(middle4)
+
+    plot_df = plot_df[plot_df['dataset_name'].isin(selected_datasets)]
+
+    order = plot_df[['dataset_name', 'median_slope']].drop_duplicates().sort_values('median_slope', ascending=False)
+    # Rename 'slope' to 'steerability' 
+    plot_df = plot_df.rename(columns = {'slope': 'steerability'})
+
+    # Plot
+    ax = axs[0]
+    sns.violinplot(plot_df, x='steerability', y = 'dataset_name', hue='dataset_name', ax=ax, order = order['dataset_name'])
+    ax.axvline(x = 0, color = 'black', linestyle = '--')
+    ax.set_title("Per-sample steerability")
+
+    # Fraction of anti-steerable examples
+    plot_df['sign_slope_mean'] = plot_df.groupby('dataset_name')['sign_slope'].transform('mean')
+    plot_df['frac_anti_steerable'] = 1 - plot_df['sign_slope_mean']
+    plot_df = plot_df[['dataset_name', 'frac_anti_steerable']].drop_duplicates()
+    # Rename 
+    plot_df = plot_df.rename(columns = {'dataset_name': 'Dataset', 'frac_anti_steerable': 'Fraction Anti-Steerable'})
+
+    # Plot
+    ax = axs[1]
+    sns.barplot(plot_df, y='Dataset', x = 'Fraction Anti-Steerable', ax=ax, order = order['dataset_name'])
+    ax.set_title("Anti-steerable examples")
+
+    # Finish
+    
+    fig.tight_layout()
+    fig.savefig('figures/fraction_anti_steerable_selected.png')
+
+plot_per_sample_steerability_and_fraction_anti_steerable_selected(df)
+
 
 # %%
 # Bar chart of slope vs response_is_A
@@ -275,13 +359,13 @@ plot_response_is_Yes(df)
 
 # %%
 # The above two in the same figure
-def plot_slope_and_counts_for_response_is_Yes(df):
+def plot_slope_and_counts_for_response_is_Yes(df, selected_only: bool = False):
 
     fig, axs = plt.subplots(
         nrows = 1, 
         ncols = 2, 
         width_ratios=[5, 1], 
-        figsize=(10, 10), 
+        figsize=(8, 5), 
         sharey=True
     )
     ax = axs[0]
@@ -289,8 +373,19 @@ def plot_slope_and_counts_for_response_is_Yes(df):
     plot_df = df[
         (df['steering_label'] == 'baseline') &
         (df['dataset_label'] == 'baseline') 
-        # & (df['dataset_name'].isin(selected_datasets.index))
     ]
+    if selected_only:
+        plot_df = plot_df[(plot_df['dataset_name'].isin(selected_datasets))]
+
+    # Fix some artefacts of preprocessing
+    # NOTE: The corrigible-neutral-HHH dataset has lots of examples where the answer starts with "Yes" but is in fact longer
+    # We don't want to count these as "Yes" responses
+
+    # For all rows where dataset == corrigible-neutral-HHH: set pos_option_is_Yes to False. 
+    plot_df.loc[plot_df['dataset_name'] == 'corrigible-neutral-HHH', 'pos_option_is_Yes'] = False
+    # Same for self-awareness-training-web-gpt
+    plot_df.loc[plot_df['dataset_name'] == 'self-awareness-training-web-gpt', 'pos_option_is_Yes'] = False
+
     # Rename 
     plot_df = plot_df.rename(columns = {'slope': 'Steerability', 'dataset_name': 'Dataset'})
 
@@ -298,7 +393,7 @@ def plot_slope_and_counts_for_response_is_Yes(df):
     plot_df['pos_Yes'] = plot_df['pos_option_is_Yes'].apply(lambda x: 'Yes' if x else 'No')
     plot_df['Positive Option'] = plot_df['pos_A'] + ' and ' + plot_df['pos_Yes']
 
-    order = plot_df[['Dataset', 'median_slope']].drop_duplicates().sort_values('median_slope', ascending=False)
+    order = plot_df[['Dataset', 'median_slope']].drop_duplicates().sort_values('median_slope', ascending=True)
     hue_order = ['A and No', 'A and Yes', 'B and No', 'B and Yes']
     sns.barplot(
         data=plot_df, 
@@ -318,6 +413,8 @@ def plot_slope_and_counts_for_response_is_Yes(df):
         (df['dataset_label'] == 'baseline') & 
         (df['multiplier'] == 0)
     ] 
+    if selected_only:
+        plot_df = plot_df[(plot_df['dataset_name'].isin(selected_datasets))]
 
     # Rename 
     plot_df = plot_df.rename(columns = {'slope': 'Steerability', 'dataset_name': 'Dataset'})
@@ -327,16 +424,30 @@ def plot_slope_and_counts_for_response_is_Yes(df):
 
     # Plot a stacked barplot of the fraction of A vs B responses in each dataset.
     count_df = plot_df.groupby(['Dataset', 'Positive Option']).size().unstack().fillna(0)
-    count_df.plot(kind='barh', stacked=True, ax = ax)
+    # Set order by order
+    count_df = count_df.loc[order['Dataset']]
+    # Set color by hue
+    colormap = sns.color_palette('tab10', n_colors=4)
+    colors = {
+        'A and No': colormap[0],
+        'A and Yes': colormap[1],
+        'B and No': colormap[2],
+        'B and Yes': colormap[3]
+    }
+
+
+    count_df.plot(kind='barh', stacked=True, ax = ax, color = [colors[col] for col in count_df.columns])
     ax.set_ylabel('Dataset')
     ax.set_xlabel('Count')
     ax.set_title('Option Counts')
     ax.get_legend().remove()
     fig.tight_layout()
-    fig.savefig("figures/plot_slope_and_counts_for_response_is_Yes.png")
+    selected_str = "_selected" if selected_only else ""
+    fig.savefig(f"figures/plot_slope_and_counts_for_response_is_Yes{selected_str}.png")
     plt.show()
 
 plot_slope_and_counts_for_response_is_Yes(df)
+plot_slope_and_counts_for_response_is_Yes(df, selected_only=True)
 
 
 # %% 
@@ -412,6 +523,7 @@ def plot_variance(df):
     variance_df = variance_df.set_index('Dataset')
     # Fix order to Unexplained, Marginal Var Explained, Var Explained
     variance_df = variance_df[['Unexplained', 'Marginal Var Explained: Yes/No', 'Var Explained: A/B']]
+    variance_df.sort_values('Unexplained', ascending=True, inplace=True)
     variance_df.plot(kind='barh', stacked=True, ax = ax)
     fig.tight_layout()
     fig.savefig('figures/breakdown_variance_explained_by_spurious_factors.png')
--- repepo/paper/make_figures_steering_ood.py
+++ repepo/paper/make_figures_steering_ood.py
@@ -135,6 +135,207 @@ def compute_steerability_id_vs_ood(df):
 compute_steerability_id_vs_ood(df)
 
 # %%
+
+def compute_steerability_df(df, model_name: str):
+    """ Get a dataframe with various ID / OOD settings. """
+    # Calculate overall steerability by dataset. 
+    # Calculate steerability within each flavour
+    mean_slope = df.groupby(['dataset_name', 'steering_label', 'dataset_label'])['slope'].mean()
+    df = df.merge(mean_slope, on=['dataset_name', 'steering_label', 'dataset_label'], suffixes=('', '_mean'))
+
+    # BASE -> BASE
+    steerability_id_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'baseline')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_id_df = steerability_id_df.rename(columns={'slope_mean': 'steerability'})
+
+    # SYS_POS -> SYS_NEG
+    steerability_ood_df = df[
+        (df.steering_label == 'SYS_positive')
+        & (df.dataset_label == 'SYS_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_df = steerability_ood_df.rename(columns={'slope_mean': 'steerability'})
+
+    # BASE -> USER_NEG
+    steerability_base_to_user_neg_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_base_to_user_neg_df = steerability_base_to_user_neg_df.rename(columns={'slope_mean': 'steerability_base_to_user_neg'})
+
+    # BASE -> USER_POS
+    steerability_base_to_user_pos_df = df[
+        (df.steering_label == 'baseline')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_base_to_user_pos_df = steerability_base_to_user_pos_df.rename(columns={'slope_mean': 'steerability_base_to_user_pos'})
+
+    # SYS_POS -> USER_NEG
+    steerability_ood_to_user_neg_df = df[
+        (df.steering_label == 'SYS_positive')
+        & (df.dataset_label == 'PT_negative')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_to_user_neg_df = steerability_ood_to_user_neg_df.rename(columns={'slope_mean': 'steerability_ood_to_user_neg'})
+
+    # SYS_NEG -> USER_POS
+    steerability_ood_to_user_pos_df = df[
+        (df.steering_label == 'SYS_negative')
+        & (df.dataset_label == 'PT_positive')
+        & (df.multiplier == 0)
+    ][['dataset_name', 'slope_mean']].drop_duplicates()
+    # Rename 'slope_mean' to 'steerability'
+    steerability_ood_to_user_pos_df = steerability_ood_to_user_pos_df.rename(columns={'slope_mean': 'steerability_ood_to_user_pos'})
+
+    # Merge the dataframes
+    steerability_df = steerability_id_df.merge(steerability_ood_df, on='dataset_name', suffixes=('_id', '_ood'))
+    steerability_df = steerability_df.merge(steerability_base_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_base_to_user_pos_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_neg_df, on='dataset_name')
+    steerability_df = steerability_df.merge(steerability_ood_to_user_pos_df, on='dataset_name')
+
+    print(steerability_df.columns)
+
+    # Save the dataframe for plotting between models
+    steerability_df.to_parquet(f'{model_name}_steerability_summary.parquet.gzip', compression='gzip')
+    steerability_df = steerability_df.rename(columns={
+        'steerability_id': 'BASE -> BASE',
+        'steerability_ood': 'SYS_POS -> SYS_NEG',
+        'steerability_base_to_user_neg': 'BASE -> USER_NEG',
+        'steerability_base_to_user_pos': 'BASE -> USER_POS',
+        'steerability_ood_to_user_neg': 'SYS_POS -> USER_NEG',
+        'steerability_ood_to_user_pos': 'SYS_NEG -> USER_POS',
+    })
+    
+    return steerability_df
+
+def make_id_vs_ood_steerability_plot_for_df(df, ax):
+
+    sns.lineplot(data=df, x='BASE -> BASE', y='BASE -> BASE', color='black', linestyle='--')
+
+    # Axes
+    ax.axhline(0, color='black', linestyle='-')
+    ax.axvline(0, color='black', linestyle='-')
+
+    # ID vs OOD
+    def make_plot_for_dist_shift(df, ax, dist_shift: str):
+        # Label is raw string with -> replaced by right arrow
+        label = dist_shift.replace(' -> ', r' $\rightarrow$ ')
+        sns.regplot(data=df, x='BASE -> BASE', y=dist_shift, scatter = False, ax = ax)
+        sns.scatterplot(data=df, x='BASE -> BASE', y=dist_shift, label=label, ax = ax)
+
+    # make_plot_for_dist_shift('SYS_POS -> SYS_NEG')
+    make_plot_for_dist_shift(df, ax, 'BASE -> USER_NEG')
+    make_plot_for_dist_shift(df, ax, 'BASE -> USER_POS')
+    make_plot_for_dist_shift(df, ax, 'SYS_POS -> USER_NEG')
+    make_plot_for_dist_shift(df, ax, 'SYS_NEG -> USER_POS')
+
+# %%
+# Load data for both models
+
+llama_df = pd.read_parquet('llama7b_steerability.parquet.gzip').drop_duplicates()
+llama_steerability_df = compute_steerability_df(llama_df, 'llama7b')
+
+qwen_df = pd.read_parquet('qwen_steerability.parquet.gzip').drop_duplicates()
+qwen_steerability_df = compute_steerability_df(qwen_df, 'qwen')
+
+
+# %%
+# Plot ID vs OOD steerability for Qwen and Llama
+plt.rcParams.update({'font.size': 30})
+fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize=(9, 6), sharey = True, sharex = True)
+
+# Llama plot
+ax = axs[0]
+
+make_id_vs_ood_steerability_plot_for_df(llama_steerability_df, ax)
+ax.set_xlabel(r'ID (BASE $\rightarrow$ BASE)')
+ax.set_ylabel('OOD')
+ax.set_title(f'Llama-2-7b-Chat')
+
+# Qwen plot
+ax = axs[1]
+
+make_id_vs_ood_steerability_plot_for_df(qwen_steerability_df, ax)
+ax.set_xlabel(r'ID (BASE $\rightarrow$ BASE)')
+ax.set_ylabel('OOD')
+ax.set_title(f'Qwen-1.5-14b-Chat')
+
+# Global legend
+handles, labels = ax.get_legend_handles_labels()
+# Fig legend overhead, but without covering the titles!
+# Fancy box
+fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 0.05), ncol=2, fancybox=True, shadow=True)
+
+# Remove the legends from the individual plots
+axs[0].get_legend().remove()
+axs[1].get_legend().remove()
+
+fig.tight_layout()
+fig.savefig(f'figures/qwen_and_llama2_steerability_id_vs_ood.png')
+
+
+# %%
+# Correlate ID and OOD steerability between models
+combined_df = llama_steerability_df.merge(qwen_steerability_df, on='dataset_name', suffixes=('_llama7b', '_qwen'))
+plt.rcParams.update({'font.size': 30})
+fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize=(9, 6), sharey = True, sharex = True)
+
+# ID steerability
+ax = axs[0] 
+sns.regplot(data=combined_df, x='BASE -> BASE_llama7b', y='BASE -> BASE_qwen', ax = ax)
+# Draw the x = y dotted line
+x = combined_df['BASE -> BASE_llama7b']
+y = combined_df['BASE -> BASE_qwen']
+min = x.min() if x.min() < y.min() else y.min()
+max = x.max() if x.max() > y.max() else y.max()
+ax.plot([min, max], [min, max], color='black', linestyle='--')
+
+ax.set_xlabel('Llama-2-7b-Chat')
+ax.set_ylabel('Qwen-1.5-14b-Chat')
+ax.set_title('ID')
+
+# OOD steerability
+ax = axs[1] 
+# Draw the x = y dotted line
+x = combined_df['BASE -> BASE_llama7b']
+y = combined_df['BASE -> BASE_qwen']
+min = x.min() if x.min() < y.min() else y.min()
+max = x.max() if x.max() > y.max() else y.max()
+ax.plot([min, max], [min, max], color='black', linestyle='--')
+
+
+sns.regplot(data=combined_df, x='SYS_POS -> USER_NEG_llama7b', y='SYS_POS -> USER_NEG_qwen', ax = ax)
+ax.set_xlabel('Llama-2-7b-Chat')
+ax.set_ylabel('Qwen-1.5-14b-Chat')
+ax.set_title('OOD')
+
+# Global legend
+handles, labels = ax.get_legend_handles_labels()
+# Fig legend overhead, but without covering the titles!
+# Fancy box
+fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 0.05), ncol=2, fancybox=True, shadow=True)
+
+# Remove the legends from the individual plots
+# axs[0].get_legend().remove()
+# axs[1].get_legend().remove()
+
+fig.tight_layout()
+fig.savefig(f'figures/correlation_steerability_between_models.png')
+
+
+
+# %%
 def compute_steerability_variance_id_vs_ood(df):
 
     # Calculate overall steerability by dataset. 
